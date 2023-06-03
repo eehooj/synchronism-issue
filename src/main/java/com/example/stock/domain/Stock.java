54ee2235ade8db3debdd266cc7372908ea1dc770
정주희 <jeongjuhui@jeongjuhuiui-MacBookAir.local>
2023-06-03 23:44:08 +0900

    first commit

---
+++ src/main/java/com/example/stock/domain/Stock.java
@@ -0,0 +1,37 @@
+package com.example.stock.domain;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.Getter;
+
+@Entity
+@Getter
+public class Stock {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private Long productId;
+
+    private Long quantity;
+
+    public Stock() {
+
+    }
+
+    public Stock(Long productId, Long quantity) {
+        this.productId = productId;
+        this.quantity = quantity;
+    }
+
+    public void decrease(Long quantity) {
+        if (this.quantity - quantity < 0) {
+            throw new RuntimeException("재고 이슈!!!");
+        }
+
+        this.quantity = this.quantity - quantity;
+    }
+}
