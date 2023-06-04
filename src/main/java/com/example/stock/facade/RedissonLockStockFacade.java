e271ea5b23722932d878cd97066b9dc0abd8a1af
joohee_jeong <jooh21020@gmail.com>
2023-06-04 21:37:20 +0900

    정리

--- src/main/java/com/example/stock/facade/RedissonLockStockFacade.java
+++ src/main/java/com/example/stock/facade/RedissonLockStockFacade.java
@@ -52,7 +52,7 @@ Lettuce
 구현이 간단하다
 spring data redis 를 이용하면 lettuce 가 기본이기때문에 별도의 라이브러리를 사용하지 않아도 된다.
 spin lock 방식이기때문에 동시에 많은 스레드가 lock 획득 대기 상태라면 redis 에 부하가 갈 수 있다.
-
+리
 Redisson
 락 획득 재시도를 기본으로 제공한다.
 pub-sub 방식으로 구현이 되어있기 때문에 lettuce 와 비교했을 때 redis 에 부하가 덜 간다.
